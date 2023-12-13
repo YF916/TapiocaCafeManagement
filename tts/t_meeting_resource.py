547c4486ab255c57cd5565495a8b3e35d3a16ddd
YF916 <yifanli0916@gmail.com>
2023-12-13 13:16:42 -0500

    add another test

---
+++ tts/t_meeting_resource.py
@@ -0,0 +1,34 @@
+import pytest
+from unittest.mock import MagicMock
+from datetime import datetime
+from resources.meetings.meeting_models import MeetingRspModel, MeetingModel
+from resources.meetings.meetings_resource import MeetingsResource
+
+@pytest.fixture
+def mock_data_service():
+    return MagicMock()
+
+@pytest.fixture
+def meetings_resource(mock_data_service):
+    config = {"data_service": mock_data_service}
+    return MeetingsResource(config)
+
+def test_get_meetings_no_id(meetings_resource, mock_data_service):
+    mock_data_service.get_meetings.return_value = [{"MeetingID": 1, "StaffID": 2, "ScheduledTime": datetime.now(), "Agenda": "Test Agenda"}]
+    meetings = meetings_resource.get_meetigs()
+    assert len(meetings) == 1
+    assert isinstance(meetings[0], MeetingRspModel)
+    mock_data_service.get_meetings.assert_called_once_with(None)
+
+def test_get_meetings_with_id(meetings_resource, mock_data_service):
+    MeetingID = 1
+    mock_data_service.get_meetings.return_value = [{"MeetingID": MeetingID, "StaffID": 2, "ScheduledTime": datetime.now(), "Agenda": "Test Agenda"}]
+    meetings = meetings_resource.get_meetigs(MeetingID)
+    assert len(meetings) == 1
+    assert meetings[0].MeetingID == MeetingID
+    mock_data_service.get_meetings.assert_called_once_with(MeetingID)
+
+
+# Add tests for change_agenda and delete_meeting methods
+
+# More tests can be added to cover all the functionalities and edge cases.
